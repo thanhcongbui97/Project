f7212d3cddeb30cc84227d9cbafc4724cc55d3dc
thanhcongbui97 <thanhcongbui.ptit@gmail.com>
2019-12-23 03:48:10 -0800

    My Project

---
+++ src/cli.cpp
@@ -0,0 +1,202 @@
+#include <sys/types.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <arpa/inet.h>
+#include "opencv2/opencv.hpp"
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+using namespace cv;
+
+#define M_ADDR "226.1.1.1"
+#define SRV_PORT 2612
+#define CLI_PORT 1997
+void display(VideoCapture cap, Mat img, Mat imgGray);
+
+int main(int argc, char *argv[])
+
+{
+	struct sockaddr_in cli_localAddr;
+
+	struct sockaddr_in srv_localAddr;
+	
+	struct ip_mreq groupSock;
+
+	int sd;
+
+	int datalen;
+
+	char databuf[1024];
+
+	unsigned int len;
+
+    /* Create a datagram socket on which to receive. */
+
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    if (sd < 0)
+
+    {
+
+        perror("Opening datagram socket error");
+
+        exit(1);
+    }
+
+    else
+
+        printf("Opening datagram socket....OK.\n");
+
+    /* Enable SO_REUSEADDR to allow multiple instances of this */
+
+    /* application to receive copies of the multicast datagrams. */
+
+    {
+
+        int reuse = 1;
+
+        if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (char *)&reuse, sizeof(reuse)) < 0)
+
+        {
+
+            perror("Setting SO_REUSEADDR error");
+
+            close(sd);
+
+            exit(1);
+        }
+
+        else
+
+            printf("Setting SO_REUSEADDR...OK.\n");
+    }
+
+    /* Bind to the proper port number with the IP address */
+
+    /* specified as INADDR_ANY. */
+
+    memset((char *)&cli_localAddr, 0, sizeof(cli_localAddr));
+
+    cli_localAddr.sin_family = AF_INET;
+
+    cli_localAddr.sin_port = htons(SRV_PORT);
+
+    cli_localAddr.sin_addr.s_addr = INADDR_ANY;
+	/*
+    memset((char *)&srv_localAddr, 0, sizeof(srv_localAddr));
+    srv_localAddr.sin_family = AF_INET;
+
+    srv_localAddr.sin_port = htons(SRV_PORT);
+
+    srv_localAddr.sin_addr.s_addr = inet_addr("127.0.0.1");*/
+    if (bind(sd, (struct sockaddr *)&cli_localAddr, sizeof(cli_localAddr)))
+
+    {
+
+        perror("Binding datagram socket error");
+
+        close(sd);
+
+        exit(1);
+    }
+
+    else
+
+        printf("Binding datagram socket...OK.\n");
+
+    /* Join the multicast group 226.1.1.1 on the local 203.106.93.94 */
+
+    /* interface. Note that this IP_ADD_MEMBERSHIP option must be */
+
+    /* called for each local interface over which the multicast */
+
+    /* datagrams are to be received. */
+
+    groupSock.imr_multiaddr.s_addr = inet_addr(M_ADDR);
+
+    groupSock.imr_interface.s_addr = inet_addr("127.0.0.1");
+
+    if (setsockopt(sd, IPPROTO_IP, IP_ADD_MEMBERSHIP, (char *)&groupSock, sizeof(groupSock)) < 0){
+
+        perror("Adding multicast group error");
+
+        close(sd);
+
+        exit(1);
+    }
+
+    else
+
+        printf("Adding multicast group...OK.\n");
+
+    /* Read from the socket. */
+
+    datalen = sizeof(databuf);
+	while(1)
+	{
+		if(read(sd, databuf, datalen) < 0)
+
+		{	
+
+		    perror("Reading datagram message error");
+			close(sd);
+		    exit(1);
+		}
+		else
+		{
+			printf("Reading datagram packet...OK.\n");
+		//	printf("Receiving packet...= %s\n",databuf);
+			cout<<"packet = "<<databuf<<endl;
+		}
+	}
+/*Open CV 
+    Mat img;
+    img = Mat::zeros(480 , 640, CV_8UC1);
+    int imgSize = img.total() * img.elemSize();
+    uchar *iptr = img.data;
+    int bytes = 0;
+    int key;
+
+    //make img continuos
+    if ( ! img.isContinuous() ) {
+          img = img.clone();
+    }
+
+    std::cout << "Image Size:" << imgSize << std::endl;
+
+
+ 	 namedWindow("TV",1);
+
+	len = (socklen_t)sizeof(srv_localAddr);
+    while(key != 'q') 
+	{
+
+      //  if ((bytes = recvfrom(sd, iptr, imgSize , 0, (struct sockaddr *)&srv_localAddr, &len) < 0)) {
+	if((bytes = read(sd, iptr, imgSize)) == -1 )
+	{        
+    	std::cerr << "recv failed, received bytes = " << bytes << std::endl;
+	break;
+        }
+	cout << "imgSize = " << bytes << endl;
+        imshow("TV", img);
+
+        // Press  ESC on keyboard to exit
+            char c = (char)waitKey(25);
+            if (c == 27)
+                break;
+        }
+
+
+        // Closes all the frames
+        destroyAllWindows();
+
+    close(sd); */ 
+	VideoCapture cap("a.mp4");
+	Mat img, imgGray;
+	display(cap, img , imgGray);
+	    
+	return 0;
+}
---
+++ src/srv.cpp
@@ -0,0 +1,199 @@
+#include <sys/types.h>
+#include <string.h>
+#include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+#define M_ADDR "226.1.1.1"
+#define PORT 2612
+
+
+struct in_addr localInterface;
+
+struct sockaddr_in groupSock;
+
+int sd;
+
+char databuf[1024] = "HI, I'm Cong";
+
+int datalen = sizeof(databuf);
+
+VideoCapture cap("a.mp4");
+Mat img, imgGray;
+
+void display(VideoCapture cap, Mat img, Mat imgGray);
+
+int main(int argc, char *argv[])
+{
+    /* Create a datagram socket on which to send. */
+    sd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sd < 0)
+    {
+        perror("Opening datagram socket error");
+        exit(1);
+    }
+    else
+        printf("Opening the datagram socket...OK.\n");
+    /* Initialize the group sockaddr structure with a */
+
+    /* group address of 226.1.1.1 and port 2612. */
+
+    memset((char *)&groupSock, 0, sizeof(groupSock));
+
+    groupSock.sin_family = AF_INET;
+
+    groupSock.sin_addr.s_addr = inet_addr(M_ADDR);
+
+    groupSock.sin_port = htons(PORT);
+
+    /* Disable loopback so you do not receive your own datagrams. */
+
+    {
+
+        char loopch = 0;
+
+        if (setsockopt(sd, IPPROTO_IP, IP_MULTICAST_LOOP, (char *)&loopch, sizeof(loopch)) < 0)
+
+        {
+
+            perror("Setting IP_MULTICAST_LOOP error");
+
+            close(sd);
+
+            exit(1);
+        }
+
+        else
+
+            printf("Disabling the loopback...OK.\n");
+    }
+
+    /* Set local interface for outbound multicast datagrams. */
+
+    /* The IP address specified must be associated with a local, */
+
+    /* multicast capable interface. */
+
+    localInterface.s_addr = inet_addr("127.0.0.1");
+
+    if (setsockopt(sd, IPPROTO_IP, IP_MULTICAST_IF, (char *)&localInterface, sizeof(localInterface)) < 0)
+
+    {
+
+        perror("Setting local interface error");
+
+        exit(1);
+    }
+
+    else
+
+        printf("Setting the local interface...OK\n");
+
+    /* Send a message to the multicast group specified by the*/
+
+    /* groupSock sockaddr structure. */
+
+    /*int datalen = 1024;*/
+	printf("Reading video frame...\n");
+	printf("Streaming packet to Multicast Group ID: %s\n...\n",M_ADDR);
+  if (sendto(sd, databuf, datalen, 0, (struct sockaddr *)&groupSock, sizeof(groupSock)) < 0)
+
+    {
+        perror("Sending packet error");
+    }
+	int i = 1;
+	char *c = (char*)malloc(sizeof(int));
+	while(i)
+	{
+		sprintf(c,"%d",i);
+		if (sendto(sd,c, 4, 0, (struct sockaddr *)&groupSock, sizeof(groupSock)) < 0)
+    {
+        perror("Sending packet error");
+    }
+		cout<<"Sent"<<c<<endl;
+		i++;
+		sleep(1);
+	}
+
+// Create a VideoCapture object and open the input file
+  // If the input is the web camera, pass 0 instead of the video file name
+  //VideoCapture cap("a.mp4"); 
+
+  // Check if camera opened successfully
+/*  if(!cap.isOpened()){
+/*    cout << "Error opening video stream or file" << endl;
+  }
+	int imgSize = img.total() * img.elemSize();
+	
+    //Mat frame, imgGray;
+  while(1){
+
+    // Capture frame-by-frame
+    cap >> img;
+	cout <<" imgSize = "<< imgSize<<endl;
+  //cap.read(frame);
+        cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+    // If the frame is empty, break immediately
+    if (img.empty())
+      break;
+
+    // Display the resulting frame
+   // imshow( "Frame", imgGray );
+if (sendto(sd, imgGray.data, 307200, 0, (struct sockaddr *)&groupSock, sizeof(groupSock)) < 0)
+		{
+			cout<<"Sent failed"<<endl;
+		}
+    // Press  ESC on keyboard to exit
+    char c=(char)waitKey(25);
+    if(c==27)
+      break;
+  }
+
+  // When everything done, release the video capture object
+  cap.release();
+
+  // Closes all the frames
+  destroyAllWindows();
+  */ return 0;
+}
+/*void display()
+{
+	VideoCapture cap("a.mp4");
+	Mat img, imgGray;
+	img = Mat::zeros(480, 640, CV_8UC1);
+	int imgSize = img.total() * img.elemSize();
+	int bytes = 0;
+	int key;
+	//make img continuos
+	if (!img.isContinuous())
+	{
+	    img = img.clone();
+	    imgGray = img.clone();
+	}
+	while (1)
+	{
+	    cap >> img;
+	    //do video processing here
+	    cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+	    //send processed image
+//	cout << "data = "<<imgGray.data<<endl<<"size = " <<imgSize<< endl;
+	if(img.empty())
+		break;	
+	imshow("SR",imgGray);
+	    if (sendto(sd, imgGray.data, imgSize, 0, (struct sockaddr *)&groupSock, sizeof(groupSock)) < 0)
+	    {
+		cerr << "bytes = " << bytes << endl;
+		//break;
+	    }*/
+/*	}	
+}
+*/
